e01c2886d01be8c9844397bacf921b9540e2f737
nesscollege2017 <nesscollege2017@gmail.com>
2017-03-03 12:52:17 +0200

    utils with access modifiers

--- src/edu/tomer/java/utils/RandomUtils.java
+++ src/edu/tomer/java/utils/RandomUtils.java
@@ -10,7 +10,7 @@ public class RandomUtils {
 
     public static int[] nextRandomArray(int min, int max, int size){
         int[] result = new int[size];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {//
             result[i] = nextRandom(min, max);
         }
         return result;
